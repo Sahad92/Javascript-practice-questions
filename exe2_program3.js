ff2b764504bec1a733a379680d58481bfd8a7726
Sahad <sahadjak92@gmail.com>
2024-11-23 16:21:45 +0530

    exercise 2 program3

---
+++ exe2_program3.js
@@ -0,0 +1,39 @@
+/*
+Program 3:
+Create a simple calculator that:
+	•	Takes two numbers as input.
+	•	Performs addition, subtraction, multiplication, and division.
+	•	Rounds the results to two decimal places.
+	•	Displays the results in a localized string format.
+
+Topics covered: arithmetic operations, toFixed(), toLocaleString()
+*/
+
+const prompt = require('prompt-sync')();
+
+let num1 = prompt("Enter a first number:");
+// // console.log(num1);
+let num2 = prompt("Enter a second number:");
+// // console.log(num2);
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+let add = Number(num1+num2)
+add.toFixed(2);
+console.log(`Additon: ${add.toLocaleString('en-IN')}`);
+
+let sub = Number(num1-num2)
+sub.toFixed(2);
+console.log(`Subraction: ${sub.toLocaleString('en-IN')}`);
+
+let mul = Number(num1*num2)
+mul.toFixed(2);
+console.log(`Multiplication: ${mul.toLocaleString('en-IN')}`);
+
+let div = Number(num1/num2)
+div.toFixed(2);
+console.log(`Division: ${div.toLocaleString('en-IN')}`);
+
+
+
